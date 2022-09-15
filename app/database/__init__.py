e059114f3bc96f9d49f7971521e85974fc259d07
EddieReynosoR <eduardo_3reynoso@hotmail.com>
2022-09-14 20:06:50 -0700

    Class 4 Code

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -7,4 +7,4 @@ def get_db():
     db = getattr(g, "_database", None)
     if not db:
         db = g._database = sqlite3.connect(DATABASE_URL)
-    retur
+    return db
--- app/routes.py
+++ app/routes.py
@@ -3,23 +3,47 @@ from flask import (
     request
 )
 
+from app.database import data_types
+
 app = Flask(__name__)
 
-@app.get("/")
+@app.get("/data_types")
 def index():
-    pass
+    out = data_types.scan()
+    return out
 
-@app.get("/integers")
+@app.get("/data_types/integers")
 def integers():
-    out = data_types.select_by_type(type="int")
+    out = data_types.select_by_type(name="Integers")
     return out
 
-@app.get("/floats")
+@app.get("/data_types/floats")
 def floats():
-    out = data_types.select_by_type(type="float")
+    out = data_types.select_by_type(name="Float")
     return out
 
-@app.get("/booleans")
+@app.get("/data_types/booleans")
 def booleans():
-    out = data_types.select_by_type(type="bool")
+    out = data_types.select_by_type(name="Booleans")
     return out
+
+
+@app.post("/data_types")
+def create_data_type():
+    dt_body = request.json
+    data_types.create(dt_body)
+
+    return "", 204
+
+@app.put("/data_types/<int:pk>")
+def update_data_type(pk):
+    dt_body = request.json
+    data_types.update(dt_body, pk)
+
+    return "", 204
+
+@app.delete("/data_types/<int:pk>")
+def delete_data_type(pk):
+    data_types.delete(pk)
+    
+    return "", 204
